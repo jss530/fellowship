16daf4bac14c119de394d19d245cee21d2549bb5
jss530 <jsifre01@gmail.com>
2018-06-13 17:24:36 -0400

    Solved q3

--- question-3.js
+++ question-3.js
@@ -1,37 +1,31 @@
-changePossibilities(amount,amount): Your quirky boss collects rare, old coins.
-They found out you're a programmer and asked you to solve something they've been wondering for a long time.
 
-Write a function that, given an amount of money and an array of coin denominations,
-computes the number of ways to make the amount of money with coins of the available denominations.
+function changePossibilities(denominations, amount) {
+    var results = [];
 
-Example: for amount=4 (4¢) and denominations=[1,2,3] (1¢, 2¢ and 3¢), your program would
-output 4—the number of ways to make 4¢ with those denominations:
+    if(denominations == null || denominations.length == 0) return results;
 
-1¢, 1¢, 1¢, 1¢
-1¢, 1¢, 2¢
-1¢, 3¢
-2¢, 2¢
+    var current = [];
+    denominations.sort();
 
-function changePossibilities(amount, denominations, partial) {
-    var s, n, remaining;
+    combinationSum(denominations, amount, 0, current, results);
 
-    partial = partial || [];
+    return results;
+}
 
-    s = partial.reduce(function (a, b) {
-      return a + b;
-    }, 0);
+function combinationSum(denominations, amount, j, c, results){
+   if(amount == 0){
+       var temp = c.slice();
+       results.push(temp);
+       return;
+   }
 
-    if (s === amount) {
-      console.log("%s", partial.join(","))
-    }
+   for(var i=j; i<denominations.length; i++){
+       if(amount < denominations[i])
+            return;
+       c.push(denominations[i]);
+       combinationSum(denominations, amount - denominations[i], i, c, results);
+       c.pop();
+   }
+}
 
-    if (s >= amount) {
-      return;
-    }
-
-    for (var i = 0; i < denominations.length; i++) {
-      n = denominations[i];
-      remaining = denominations.slice(i + 1);
-      changePossibilities(remaining, amount, partial.concat([n]));
-    }
-};
+changePossibilities(4, [1,2,3]);
